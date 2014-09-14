49ca12d0b839281b0b4c51d1802d8841798641e9
Duncan Bayne <duncan@bayne.id.au>
2014-09-14 11:14:18 +1000

    Duh.

--- setup-root.sh
+++ setup-root.sh
@@ -4,20 +4,20 @@ set -e
 
 echo Installing SVN...
 cd /usr/ports/devel/subversion
-make && make install
+make install
 
 echo Getting kernel sources...
 svn co svn://svn.freebsd.org/base/stable/10 /usr/src
 
 echo Installing Git...
 cd /usr/ports/devel/git
-make && make install
+make install
 
 echo Installing SBCL...
 cd /usr/ports/lang/sbcl
-make && make install
+make install
 
 echo Installing XOrg...
 cd /usr/ports/x11/xorg
-make && make install
+make install
 
