8cf217608ba80568c006223ec028746a4fcc8edc
Duncan Bayne <duncan@bayne.id.au>
2014-09-17 07:54:49 +1000

    Correctly configure Xorg.

--- setup-root.sh
+++ setup-root.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+if grep -q "037b7c29-5804-43e2-8054-d1ebfb0f3293" /etc/make.conf;
+then
+    echo Custom build options already added to /etc/make.conf.
+else
+    echo >> /etc/make.conf
+    echo \# Custom build options, added by freebsd-setup >> /etc/make.conf
+    echo \# 037b7c29-5804-43e2-8054-d1ebfb0f3293 >> /etc/make.conf
+    echo WITH_NEW_XORG=\"YES\" >> /etc/make.conf
+    echo >> /etc/make.conf
+fi
+
 echo Installing SVN...
 cd /usr/ports/devel/subversion
 make clean install
@@ -17,7 +28,6 @@ portmaster x11/xorg
 portmaster x11-drivers/xf86-input-keyboard
 portmaster x11-drivers/xf86-input-mouse
 portmaster x11/terminator
-portmaster lang/sbcl
 
 if grep -q "037b7c29-5804-43e2-8054-d1ebfb0f3293" /etc/rc.conf;
 then
@@ -30,6 +40,8 @@ else
     echo dbus_enable=\"YES\" >> /etc/rc.conf
     echo >> /etc/rc.conf
 fi
+
+portmaster lang/sbcl
 
 # TODO: install these with portmaster
 # git
