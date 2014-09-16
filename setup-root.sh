72e2ac7d5c67c35281c92793bcc4e8bad2cc239b
Duncan Bayne <duncan@bayne.id.au>
2014-09-16 16:39:38 +1000

    Trying out portmanager after suggestion from @kirillrdy.

--- setup-root.sh
+++ setup-root.sh
@@ -4,40 +4,24 @@ set -e
 
 echo Installing SVN...
 cd /usr/ports/devel/subversion
-make install
+make clean install
 
 echo Getting kernel sources...
 svn co svn://svn.freebsd.org/base/stable/10 /usr/src
 
-echo Installing Git...
-cd /usr/ports/devel/git
-make install
+echo Installing portmaster...
+cd /usr/ports/ports-mgmt/portmaster
+make clean install
 
-echo Installing SBCL...
-cd /usr/ports/lang/sbcl
-make install
-
-echo Installing Links...
-cd /usr/ports/www/links
-make install
-
-echo Installing XOrg...
-cd /usr/ports/x11/xorg
-make install
-
-echo Installing ccrypt...
-cd /usr/ports/security/ccrypt
-make install
-
-echo Installing Unison...
-cd /usr/ports/net/unison
-make install
-
-echo Installing Ruby...
-cd /usr/ports/lang/ruby
-make install
-
-echo Installing Emacs...
-cd /usr/ports/editors/emacs
-make install
+# TODO: install these with portmaster
+# git
+# sbcl
+# xorg (including hald)
+# firefox
+# chromium (including shared-memory enable)
+# links
+# ccrypt
+# unison
+# ruby21
+# emacs
 
