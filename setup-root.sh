84ee310db20fbbe599c48ee0d214c8f921d45bd1
Duncan Bayne <duncan@bayne.id.au>
2014-09-16 21:19:47 +1000

    Install Xorg (#2)

--- setup-root.sh
+++ setup-root.sh
@@ -13,10 +13,16 @@ echo Installing portmaster...
 cd /usr/ports/ports-mgmt/portmaster
 make clean install
 
+echo WITH_NEW_XORG="YES" >> /etc/make.conf
+export BATCH=yes
+
+portmaster x11/xorg
+postmaster x11-drivers/xf86-input-keyboard
+postmaster x11-drivers/xf86-input-mouse
+
 # TODO: install these with portmaster
 # git
 # sbcl
-# xorg (including hald)
 # firefox
 # chromium (including shared-memory enable)
 # links
