885feff84760da0869315fa9431b5eb305ad9732
Duncan Bayne <duncan@bayne.id.au>
2014-09-21 08:44:06 +1000

    Set up personal bin dir, ignoring scripts added by personal setup.

--- setup-user.sh
+++ setup-user.sh
@@ -12,12 +12,14 @@ then
     echo Shell confugration extras already added to ~/.profile.
 else
     echo Adding custom Shell setup to ~/profile
-    if [ ! -d ~/bin ]; then mkdir ~/bin ; fi
     echo >> ~/.profile
     echo \# Personal Shell configuration extras, added by freebsd-setup >> ~/.profile
     echo \# 037b7c29-5804-43e2-8054-d1ebfb0f3293 >> ~/.profile
     echo . ~/freebsd-setup/conf/shell/shell_extras.sh >> ~/.profile
     echo >> ~/.profile
 fi
+
+rm -f ~/bin
+ln -s ~/freebsd-setup/conf/bin ~/bin
 
 if [ ! -f ~/.stumpwmrc ]; then ln -s ~/freebsd-setup/conf/stumpwm/stumpwmrc ~/.stumpwmrc; fi
