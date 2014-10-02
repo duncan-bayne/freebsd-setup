fb9b8dae25bf0b2bc8a5bc46f8b24dffdcee3066
Duncan Bayne <duncan@bayne.id.au>
2014-10-02 20:46:41 +1000

    Ooops, missed a step.

--- setup-root.sh
+++ setup-root.sh
@@ -29,7 +29,7 @@ make clean install
 
 echo Configuring 32-bit stuff for Virtualbox...
 cd /usr/src
-make build32 install32
+make toolchain build32 install32
 /etc/rc.d/ldconfig restart
 
 echo Installing and configuring ports...
