4ac3f5a83ea341f5661ad913dd05e7915bb227fa
Duncan Bayne <duncan@bayne.id.au>
2014-10-10 17:22:40 +1100

    Install and use Oh My ZSH.  Makes custom config nicer too.

--- setup-user.sh
+++ setup-user.sh
@@ -7,18 +7,6 @@ set -e
 
 git config --global push.default matching
 
-if grep -q "037b7c29-5804-43e2-8054-d1ebfb0f3293" ~/.zshrc
-then
-    echo Shell confugration extras already added to ~/.zshrc.
-else
-    echo Adding custom Shell setup to ~.zshrc
-    echo >> ~/.zshrc
-    echo \# Personal Shell configuration extras, added by freebsd-setup >> ~/.zshrc
-    echo \# 037b7c29-5804-43e2-8054-d1ebfb0f3293 >> ~/.zshrc
-    echo . ~/freebsd-setup/conf/shell/shell_extras.sh >> ~/.zshrc
-    echo >> ~/.zshrc
-fi
-
 rm -f ~/bin
 ln -s ~/freebsd-setup/conf/bin ~/bin
 
@@ -27,5 +15,7 @@ if [ ! -f ~/.stumpwmrc ]; then ln -s ~/freebsd-setup/conf/stumpwm/stumpwmrc ~/.s
 echo Adding $USER to usb group...
 sudo pw groupmod usb -M $USER
 
-echo Switching to zsh...
+echo Switching to zsh and installing Oh My ZSH
 chsh -s /usr/local/bin/zsh
+curl -L http://install.ohmyz.sh | sh
+ln ~/freebsd-setup/conf/zsh/freebsd-setup $ZSH/custom/freebsd-setup
