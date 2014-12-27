60577e17d6d87e986304f58b520c463480d375cf
Duncan Bayne <duncan@bayne.id.au>
2014-12-27 15:19:33 +1100

    Fix FreeBSD cert bug.

--- install-rbenv.sh
+++ install-rbenv.sh
@@ -14,6 +14,11 @@ echo Installing Ruby 2.0.0 with rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install 2.0.0-p598
 rbenv global 2.0.0-p598
+rbenv rehash
+
+# fix an annoying FreeBSD cert bug
+SSL_CERT_FILE=/usr/local/etc/ssl/cert.pem gem update --system
+
 gem install bundler
 rbenv rehash
 
