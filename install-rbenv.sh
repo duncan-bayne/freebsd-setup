7aa4181856b674378dce4a6aa1f66b5e5453e171
Duncan Bayne <duncan@bayne.id.au>
2016-03-11 16:10:45 +1100

    Temporary hack around #49

--- install-rbenv.sh
+++ install-rbenv.sh
@@ -6,19 +6,5 @@ cd ~
 rm -rf ~/.rbenv
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
-# initialize rbenv so we can use it without restarting the shell
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-echo Installing Ruby 2.0.0 with rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install 2.0.0-p598
-rbenv global 2.0.0-p598
-rbenv rehash
-
-# fix an annoying FreeBSD cert bug
-SSL_CERT_FILE=/usr/local/etc/ssl/cert.pem gem update --system
-
-gem install bundler
-rbenv rehash
-
+# HACK: this is a temporary workaround for https://github.com/duncan-bayne/freebsd-setup/issues/49
+sudo pkg install -y ruby
